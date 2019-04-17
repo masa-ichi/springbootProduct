297d369446e02cf3ffede3e28d4524774f2f1304
一瀬将治 <ichinosemasaharu@team-lab.com>
2019-04-17 16:46:25 +0900

    update:直下だけど画像をアップロードできるようにした

---
+++ src/main/java/com/sample/api/FileUploadController.java
@@ -0,0 +1,44 @@
+package com.sample.api;
+
+
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@RestController
+public class FileUploadController {
+
+
+    @RequestMapping(value = "api/upload", method= RequestMethod.POST)
+    public void handle(
+            HttpServletResponse response,
+            @RequestParam MultipartFile file
+    ){
+        // ファイルが空の場合は HTTP 400 を返す。
+        if (file.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        // アップロードされたファイルを保存。
+        try {
+            BufferedInputStream in =
+                    new BufferedInputStream(file.getInputStream());
+            BufferedOutputStream out =
+                    new BufferedOutputStream(
+                            new FileOutputStream(file.getOriginalFilename()));
+            FileCopyUtils.copy(in, out);
+        } catch (IOException e) {
+            throw new RuntimeException("Error uploading file.", e);
+        }
+    }
+
+}
