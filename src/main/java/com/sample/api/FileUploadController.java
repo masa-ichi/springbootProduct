9696ce727a1f15c9cab2a1e76cc1b1bf56791bb8
一瀬将治 <ichinosemasaharu@team-lab.com>
2019-04-19 10:52:41 +0900

    updata:画像の保存先を/uploadfile内に変更

--- src/main/java/com/sample/api/FileUploadController.java
+++ src/main/java/com/sample/api/FileUploadController.java
@@ -3,7 +3,6 @@ package com.sample.api;
 
 import com.sample.service.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -23,44 +22,41 @@ public class FileUploadController {
     @Autowired
     FileUploadService fileUploadService;
 
+
     @RequestMapping(method = RequestMethod.POST)
     public void handle(
+
             HttpServletResponse response,
             @RequestParam("file") MultipartFile file
+
     ){
         // ファイルが空の場合は HTTP 400 を返す。
         if (file.isEmpty()) {
+
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
+
         }
 
-//        // ファイル種類から決まる値をセットする
-//        StringBuffer filePath = new StringBuffer("/uploadfile")
-//                .append(File.separator).append("img");   //ファイルパス
-
-//        // アップロードファイルを格納するディレクトリを作成する
-//        File uploadDir = fileUploadService.mkdirs(filePath);
-
-//        System.out.println(filePath);
+        StringBuffer filePath = new StringBuffer("./uploadfile/" + file.getOriginalFilename());
 
         // アップロードされたファイルを保存。
         try {
-//            File uploadFile = new File(String.valueOf(filePath));
 
-//            byte[] bytes = file.getBytes();
-//            BufferedOutputStream uploadFileStream =
-//                    new BufferedOutputStream(new FileOutputStream(uploadFile));
-//            uploadFileStream.write(bytes);
-//            uploadFileStream.close();
+            File uploadFile = new File(String.valueOf(filePath));
 
-            BufferedInputStream in =
-                    new BufferedInputStream(file.getInputStream());
-            BufferedOutputStream out =
-                    new BufferedOutputStream(
-                            new FileOutputStream(file.getOriginalFilename()));
-            FileCopyUtils.copy(in, out);
+            byte[] bytes = file.getBytes();
+
+            BufferedOutputStream uploadFileStream =
+                    new BufferedOutputStream(new FileOutputStream(uploadFile));
+
+            uploadFileStream.write(bytes);
+            uploadFileStream.close();
+
         } catch (IOException e) {
+
             throw new RuntimeException("Error uploading file.", e);
+
         }
     }
 
